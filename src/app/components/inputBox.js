116cc00d74356cc5aa7f72a0aaddcea9c6b9d482
shoobah <shoobah@gmail.com>
2018-01-11 09:28:29 +0100

    Component styling

--- src/app/components/inputBox.js
+++ src/app/components/inputBox.js
@@ -2,15 +2,20 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
 class InputBox extends Component {
+    constructor(props){
+        super(props)
+        this.myStyle = {
+            width: '500px',
+            border: 'solid 2px #ff0000'
+        };
+    }
     render() {
-        console.log('this.props', this.props)
-        
-        return <div>{this.props.thing}</div>;
+        return <input style={this.myStyle} placeholder={this.props.thing} />;
     }
 }
 
 InputBox.propTypes = {
-    thing: PropTypes.string,
+    thing: PropTypes.string
 };
 
 export default InputBox;
