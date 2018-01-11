c02a1e19625401e55ff0b900885efa9226a9fc0c
shoobah <shoobah@gmail.com>
2018-01-11 14:56:57 +0100

    More stuff

--- src/app/components/dogData.js
+++ src/app/components/dogData.js
@@ -1,17 +1,29 @@
 import React from 'react';
 
+const bigFont = {listStyle: 'none', fontSize: '18px', textAlign: 'left'};
+const smallFont = {listStyle: 'none', fontSize: '14px', textAlign: 'left'};
+
+const generatSubs = (message, item) => {
+    if (message[item].length === 0) return null;
+    return <ul style={smallFont}>{message[item].map((item, index) => <li key={'sub-' + index}>{item}</li>)}</ul>;
+};
+
 const generateRows = data => {
     let a = [];
     let key = 0;
     for (const item in data.message) {
-        a.push(<li key={key}>{item}</li>);
-        key++;
+        a.push(
+            <li key={key++}>
+                {item}
+                {generatSubs(data.message, item)}
+            </li>
+        );
     }
     return a;
 };
 
 const dogData = props => {
-    return <ul style={{listStyle: 'none'}}>{generateRows(props.data)}</ul>;
+    return <ul style={bigFont}>{generateRows(props.data)}</ul>;
 };
 
 export default dogData;
